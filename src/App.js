ac22ce8f8d007eb58e734e32a8631901e4d05a56
Justin Blalock <justin.m.blalock@gmail.com>
2022-01-11 20:06:31 -0600

    added SignIn component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.scss";
 import { hot } from "react-hot-loader/root";
 
+import { Container } from "react-bootstrap";
+
 import NavComponent from "../client/components/NavComponent";
+import SignInComponent from "../client/components/SignInComponent";
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <Container>
         <NavComponent />
-      </>
+        <SignInComponent />
+      </Container>
     );
   }
 }
