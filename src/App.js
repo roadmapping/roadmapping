e1ede1f616eee3d67f4fa07d16b55845279326cf
zoew McGrathe <zoewmcgrath@gmail.comm>
2022-01-11 21:59:12 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.scss";
 import { hot } from "react-hot-loader/root";
+import Route from "react-router-dom";
+//import { BrowserRouter }
 
 import NavComponent from "../client/components/NavComponent";
-
+import Dashboard from "../client/components/Dashboard.jsx";
 class App extends React.Component {
   render() {
     return (
       <>
         <NavComponent />
+        <Dashboard />
       </>
     );
   }
